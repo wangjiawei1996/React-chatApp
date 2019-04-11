6fd10f63578856f9036a6e0d9c11533f5a5a9f13
王嘉巍 <906789088@qq.com>
2019-04-11 21:53:16 +0800

    review redux by store.getState and store.dispatch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//新建store
+import { createStore } from 'redux';
+//通过reducer遍历 根据老state和action形成新的状态
+function count(state=0, action) {
+  switch(action.type) {
+    case '加机关枪':
+      return state+1;
+    case '减机关枪':
+      return state-1;
+    default:
+      return 10
+  }
+}
+const store = createStore(count)
+const init = store.getState()
+console.log(init)
+function listener() {
+  const current = store.getState()
+  console.log(`现在有机关枪${current}把`)
+}
+store.subscribe(listener)
+//派发事件 传递action
+store.dispatch({type:'加机关枪'})
+store.dispatch({type:'加机关枪'})
+store.dispatch({type:'加机关枪'})
+store.dispatch({type:'减机关枪'})
