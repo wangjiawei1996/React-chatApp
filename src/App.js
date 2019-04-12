4be15e65e87d2a951411e5c8ed8ff7330acd0494
王嘉巍 <906789088@qq.com>
2019-04-12 16:26:31 +0800

    redux store.dispatch action  and newState

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { addGUN } from './index.redux'
+class App extends React.Component{
+  // constructor(props) {
+  //   super(props)
+  // }
+  render() {
+    const store = this.props.store
+    const num = store.getState()
+
+    return (
+      <div>
+        <h1>现在有机枪{num}把</h1>
+        <button onClick={() => {store.dispatch(addGUN())}}>申请机枪</button>
+      </div>
+    )
+  }
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
-//新建store
-import { createStore } from 'redux';
-//通过reducer遍历 根据老state和action形成新的状态
-function count(state=0, action) {
-  switch(action.type) {
-    case '加机关枪':
-      return state+1;
-    case '减机关枪':
-      return state-1;
-    default:
-      return 10
-  }
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+import { createStore } from 'redux'
+import { counter } from './index.redux'; 
+const store = createStore(counter)
+function render() {
+  ReactDom.render(<App store={store} />,document.getElementById('root'))
 }
-const store = createStore(count)
-const init = store.getState()
-console.log(init)
-function listener() {
-  const current = store.getState()
-  console.log(`现在有机关枪${current}把`)
-}
-store.subscribe(listener)
-//派发事件 传递action
-store.dispatch({type:'加机关枪'})
-store.dispatch({type:'加机关枪'})
-store.dispatch({type:'加机关枪'})
-store.dispatch({type:'减机关枪'})
+render()
+store.subscribe(render)
