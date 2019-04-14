468d4185c76afcb028ebd2e7ee9f408279b24d93
王嘉巍 <906789088@qq.com>
2019-04-14 16:06:03 +0800

    add window redux devtools in compose

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import { counter,addGun,removeGun,addGunAsync } from './index.redux'; 
-const store = createStore(counter, applyMiddleware(thunk))
+import { counter,addGun,removeGun,addGunAsync } from './index.redux';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(counter, composeEnhancers(
+  applyMiddleware(thunk)
+))
 function render() {
   ReactDom.render(<App store={store} addGunAsync={addGunAsync} addGun={addGun} removeGun={removeGun}  />,document.getElementById('root'))
 }
