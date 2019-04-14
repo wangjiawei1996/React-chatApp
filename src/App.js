97e66f5c8a13861a7e44d7b1b80f9ec2ec427cf6
王嘉巍 <906789088@qq.com>
2019-04-14 17:00:46 +0800

    import react-redux  provider and connect App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addGun, removeGun, addGunAsync} from './index.redux'
 class App extends React.Component{
-  // constructor(props) {
-  //   super(props)
-  // }
   render() {
-    const store = this.props.store
-    const num = store.getState()
-    const addGun = this.props.addGun
-    const removeGun = this.props.removeGun
-    const addGunAsync = this.props.addGunAsync
-
     return (
       <div>
-        <h1>现在有机枪{num}把</h1>
-        <button onClick={() => {store.dispatch(addGun())}}>申请机枪</button>
-        <button onClick={() => {store.dispatch(removeGun())}}>上交武器</button>
-        <button onClick={() => {store.dispatch(addGunAsync())}}>拖几天再给</button>
+        <h1>现在有机枪{this.props.num}把</h1>
+        <button onClick={this.props.addGun}>申请机枪</button>
+        <button onClick={this.props.removeGun}>上交武器</button>
+        <button onClick={this.props.addGunAsync}>拖几天再给</button>
       </div>
     )
   }
 }
+const mapStatetoProps = (state) => {
+  return {num:state}
+}
+const actionCreators = { addGun, removeGun, addGunAsync}
+App = connect(mapStatetoProps, actionCreators)(App)
 export default App
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDom from 'react-dom'
 import App from './App'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import { counter,addGun,removeGun,addGunAsync } from './index.redux';
+import { Provider } from 'react-redux'
+import { counter } from './index.redux';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(counter, composeEnhancers(
   applyMiddleware(thunk)
 ))
-function render() {
-  ReactDom.render(<App store={store} addGunAsync={addGunAsync} addGun={addGun} removeGun={removeGun}  />,document.getElementById('root'))
-}
-render()
-store.subscribe(render)
+ReactDom.render(
+  (<Provider store={store}>
+    <App />
+  </Provider>),
+  document.getElementById('root')
+)
