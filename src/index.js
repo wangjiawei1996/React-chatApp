a007f091691aca83125701b52a310e926b9bf1a8
王嘉巍 <906789088@qq.com>
2019-04-16 12:09:04 +0800

    React-router add BrowserRouter, Route Link(exact)

--- src/index.js
+++ src/index.js
@@ -4,15 +4,39 @@ import App from './App'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Link} from 'react-router-dom';
 import { counter } from './index.redux';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(counter, composeEnhancers(
   applyMiddleware(thunk)
 ))
+function Erying() {
+  return <h2>二营</h2>
+}
+function Sanying() {
+  return <h2>三营</h2>
+}
 ReactDom.render(
   (<Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>一营</Link>
+          </li>
+          <li>
+            <Link to='/erying'>二营</Link>
+          </li>
+          <li>
+            <Link to='/sanying'>三营</Link>
+          </li>
+        </ul>
+      </div>
+      <Route path='/' exact component={App}></Route>
+      <Route path='/erying' component={Erying}></Route>
+      <Route path='/sanying' component={Sanying}></Route>
+    </BrowserRouter>
   </Provider>),
   document.getElementById('root')
 )
