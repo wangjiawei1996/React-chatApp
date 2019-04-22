810723cc6547ff9ddbf9ea07decb80cb59dbcbac
王嘉巍 <906789088@qq.com>
2019-04-22 10:56:03 +0800

    this.props.match catch  url and delete Redirect

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -12,6 +12,7 @@ function Sanying() {
 }
 class Dashboard extends React.Component {
   render() {
+    const match = this.props.match
     const redireToLogin = <Redirect to='/login'></Redirect>
     const app = (
       <div>
@@ -19,18 +20,18 @@ class Dashboard extends React.Component {
         { this.props.isAuth? <button onClick={this.props.logout}>注销</button> : null }
         <ul>
           <li>
-            <Link to='dashboard/'>一营</Link>
+            <Link to={`${match.url}/`}>一营</Link>
           </li>
           <li>
-            <Link to='dashboard/erying'>二营</Link>
+            <Link to={`${match.url}/erying`}>二营</Link>
           </li>
           <li>
-            <Link to='dashboard/sanying'>三营</Link>
+            <Link to={`${match.url}/sanying`}>三营</Link>
           </li>
         </ul>
-        <Route path = '/dashboard/' exact component={App}></Route>
-        <Route path = '/dashboard/erying' component={Erying}></Route>
-        <Route path = '/dashboard/sanying' component={Sanying}></Route>
+        <Route path = {`${match.url}/`} exact component={App}></Route>
+        <Route path = {`${match.url}/erying`} component={Erying}></Route>
+        <Route path = {`${match.url}/sanying`} component={Sanying}></Route>
       </div>
     )
     return this.props.isAuth ? app: redireToLogin
