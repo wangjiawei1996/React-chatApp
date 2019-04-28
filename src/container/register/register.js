0b8fc83f2455389ec7e38c3e6e901a359e0b937a
王嘉巍 <906789088@qq.com>
2019-04-28 20:25:22 +0800

    handleRegister.bind in primary

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -5,8 +5,20 @@ class Register extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      user: '',
+      pwd: '',
+      repeatpwd: '',
       type: 'genius'  // 或者boss
     }
+    this.handleRegister = this.handleRegister.bind(this)
+  }
+  handleChange(key, val) {
+    this.setState({
+      [key]: val
+    })
+  }
+  handleRegister() {
+    console.log(this.state)
   }
   render() {
     const RadioItem = Radio.RadioItem
@@ -14,19 +26,33 @@ class Register extends React.Component {
       <div>
         <Logo></Logo>
         <List>
-          <InputItem>用户名</InputItem>
+          <InputItem
+            onChange={v => this.handleChange('user', v)}
+          >用户名</InputItem>
           <WhiteSpace />
-          <InputItem>密码</InputItem>
+          <InputItem
+            type="password"
+            onChange={v => this.handleChange('pwd', v)}
+          >密码</InputItem>
           <WhiteSpace />
-          <InputItem>确认密码</InputItem>
+          <InputItem
+            type="password"
+            onChange={v => this.handleChange('repeatpwd', v)}
+          >确认密码</InputItem>
           <WhiteSpace />
-          <RadioItem checked={this.state.type === 'genius'}>
+          <RadioItem
+            checked={this.state.type === 'genius'}
+            onChange={() => this.handleChange('type','genius')}
+          >
             牛人
           </RadioItem>
-          <RadioItem checked={this.state.type === 'boss'}>
+          <RadioItem
+            checked={this.state.type === 'boss'}
+            onChange={() => this.handleChange('type','boss')}
+          >
             Boss
           </RadioItem>
-          <Button type='primary'>注册</Button>
+          <Button type='primary' onClick={this.handleRegister}>注册</Button>
         </List>
       </div>
     )
