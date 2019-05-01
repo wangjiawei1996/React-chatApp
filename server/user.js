126cd7a1430040001648b2761f38f52a8c499bd6
王嘉巍 <906789088@qq.com>
2019-05-01 19:53:09 +0800

    npm install cookieParser and userRouter in server.js get res.data

--- server/user.js
+++ server/user.js
@@ -7,6 +7,21 @@ Router.get('list', function(req, res) {
     return res.json(doc)
   })
 })
+Router.post('/register', function(req, res) {
+  console.log(req.body.data)
+  const {user, pwd, type} = req.body.data
+  User.findOne({user}, function(err, doc) {
+    if (doc) {
+      return res.json({code: 1,msg:'用户名重复'})
+    }
+    User.create({user, pwd, type},function(e, d) {
+      if (e) {
+        return res.json({code: 1, msg:'后端出错了'})
+      }
+      return res.json({code: 0})
+    })
+  })
+})
 Router.get('/info', (req, res) => {
   return res.json({code: 1})
 })
