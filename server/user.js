c02d97fae0fe6582cc620a2334e6c92dfc2f1a37
王嘉巍 <906789088@qq.com>
2019-05-04 18:48:58 +0800

    fix  bug for req.body.data dont't catch data

--- server/user.js
+++ server/user.js
@@ -8,8 +8,7 @@ Router.get('list', function(req, res) {
   })
 })
 Router.post('/register', function(req, res) {
-  console.log(req.body.data)
-  const {user, pwd, type} = req.body.data
+  const {user, pwd, type} = req.body
   User.findOne({user}, function(err, doc) {
     if (doc) {
       return res.json({code: 1,msg:'用户名重复'})
