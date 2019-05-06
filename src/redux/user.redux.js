65c2be4e455bfd8a2010e49a2aa3e075f83e618e
王嘉巍 <906789088@qq.com>
2019-05-06 15:06:16 +0800

    finish login and register check in user.js

--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 import { getRedirectPath } from '../util'
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 const ERROR_MSG = 'ERROR_MSG';
 const initState = {
   redirectTo: '',
@@ -16,6 +17,8 @@ export function user(state = initState, action) {
   switch(action.type) {
     case REGISTER_SUCCESS:
       return {...state, msg:'', redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload}
+    case LOGIN_SUCCESS:
+      return {...state, msg:'', redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload}
     case ERROR_MSG:
       return {...state, isAuth: false, msg: action.msg}
     default:
@@ -25,8 +28,26 @@ export function user(state = initState, action) {
 function registerSuccess(data) {
   return { type: REGISTER_SUCCESS, payload: data}
 }
+function loginSuccess(data) {
+  return { type: LOGIN_SUCCESS, payload: data}
+}
 function errorMsg (msg) {
   return { msg, type: ERROR_MSG }
+}
+export function login({user, pwd}) {
+  if (!user||!pwd) {
+    return errorMsg('用户名和密码必须输入')
+  }
+  return dispatch => {
+    axios.post('/user/login', {user, pwd})
+      .then(res => {
+        if (res.status === 200&&res.data.code === 0) {
+          dispatch(loginSuccess(res.data.data))
+        } else {
+          dispatch(errorMsg(res.data.msg))
+        }
+      })
+  }
 }
 export function register({user,pwd,repeatpwd,type}) {
   if (!user || !pwd || !type) {
