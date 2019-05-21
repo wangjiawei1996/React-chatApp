6f6cc5a910c97445753d13e1164df1080fafa087
王嘉巍 <906789088@qq.com>
2019-05-21 14:58:55 +0800

    build geniusinfo page and npm install Prop-types

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import Login from './container/login/login'
 import Register from './container/register/register'
 import AuthRoute from './component/authroute/authroute'
 import BossInfo from './container/bossinfo/bossinfo'
+import GeniusInfo from './container/geniusinfo/geniusinfo'
 import reducers from './reducer'
 import './config'
 import './index.css'
@@ -29,6 +30,7 @@ ReactDom.render(
       <div>
         <AuthRoute></AuthRoute>
         <Route path='/bossinfo' component = {BossInfo}></Route>
+        <Route path='/geniusinfo' component = {GeniusInfo}></Route>
         <Route path='/login' component = {Login}></Route>
         <Route path='/register' component = {Register}></Route>
       </div>
