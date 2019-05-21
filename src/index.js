54ef809a2a6e8cae68722222bab609e124d82729
王嘉巍 <906789088@qq.com>
2019-05-21 15:40:34 +0800

    add dashboard.js to finish headerbar and footerBar

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import {
   BrowserRouter,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
 import Login from './container/login/login'
 import Register from './container/register/register'
 import AuthRoute from './component/authroute/authroute'
 import BossInfo from './container/bossinfo/bossinfo'
 import GeniusInfo from './container/geniusinfo/geniusinfo'
+import Dashboard from './component/dashboard/dashboard'
 import reducers from './reducer'
 import './config'
 import './index.css'
@@ -29,10 +31,13 @@ ReactDom.render(
     <BrowserRouter>
       <div>
         <AuthRoute></AuthRoute>
-        <Route path='/bossinfo' component = {BossInfo}></Route>
-        <Route path='/geniusinfo' component = {GeniusInfo}></Route>
-        <Route path='/login' component = {Login}></Route>
-        <Route path='/register' component = {Register}></Route>
+        <Switch>
+          <Route path='/bossinfo' component = {BossInfo}></Route>
+          <Route path='/geniusinfo' component = {GeniusInfo}></Route>
+          <Route path='/login' component = {Login}></Route>
+          <Route path='/register' component = {Register}></Route>
+          <Route component = {Dashboard}></Route>
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>),
