699d1f45a1cf9d5f23ecc92422cad9f87455a447
王嘉巍 <906789088@qq.com>
2019-05-27 14:28:06 +0800

    create user page to become easily to User

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -5,11 +5,9 @@ import {Switch,Route} from 'react-router-dom'
 import NavLinkBar from '../navlink/navlink'
 import Boss from '../../component/boss/boss'
 import Genius from '../../component/genius/genius'
+import User from '../../component/user/user'
 function Msg() {
   return <h2>消息列表首页</h2>
-}
-function User() {
-  return <h2>个人中心页面</h2>
 }
 class Dashboard extends React.Component{
   render() {
---
+++ src/component/user/user.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class User extends React.Component {
+  render() {
+    return (
+      <div>用户中心</div>
+    )
+  }
+ }
+export default User;
