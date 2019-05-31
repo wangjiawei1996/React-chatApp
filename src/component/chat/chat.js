788e1c4f13daf9baa93d4f40159da251dbf666e8
王嘉巍 <906789088@qq.com>
2019-05-31 17:18:47 +0800

    componmentDidmount will socket.io work with chat finish

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,6 +1,9 @@
 import React from 'react';
-
+import io from 'socket.io-client'
 class Chat extends React.Component {
+  componentDidMount() {
+    const socket = io('ws://localhost:9093')
+  }
   render() {
     console.log(this.props)
     return (
