182c37fa46e707c6d337778c26bac1e2a3ad58ef
王嘉巍 <906789088@qq.com>
2019-06-11 16:55:35 +0800

    const unreadNum in msg.js and finish many people submit message

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { List } from 'antd-mobile';
+import { List, Badge } from 'antd-mobile';
 class Msg extends React.Component {
   getLast(arr) {
     return arr[arr.length - 1]
@@ -21,6 +21,7 @@ class Msg extends React.Component {
           {chatList.map(v => {
             const lastItem = this.getLast(v)
             const targetId = v[0].from === userid?v[0].to:v[0].from
+            const unreadNum = v.filter(v => !v.read && v.to===userid).length
             if (!userinfo[targetId]) {
               return null
             }
@@ -29,6 +30,7 @@ class Msg extends React.Component {
             return (
               <List key={lastItem._id}>
                 <Item
+                  extra = {<Badge text={unreadNum}></Badge>}
                   thumb={require(`../img/${userinfo[targetId].avatar}.png`)}
                 >
                   {lastItem.content}
